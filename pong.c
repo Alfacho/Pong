1aa65bf578dc7bc5b0433e46fa70a79e05820178
AlSi <primer.alex.si@gmail.com>
2023-02-09 18:54:43 +0200

    Начало моего проекта

---
+++ pong.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) {
+    
+    return 0;
+}
+
+int f_display() {
+
+    return 0;
+}
+
+int f_first_racket_physics() {
+    
+    return 0;
+}
+
+int f_second_racket_physics() {
+    
+    return 0;
+}
+
+int f_ball_physics() {
+
+    return 0;
+}
