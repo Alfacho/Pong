9ec2d45f2353e790fab54b2c0a429eba00fd6116
AlSi <primer.alex.si@gmail.com>
2023-02-23 13:04:59 +0200

    one step for win!

--- pong.c
+++ pong.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-#include <curses.h>
+#include <ncurses.h>
 #include <unistd.h>
+#include <termios.h>
+#include <poll.h>
 
 // graphics
 #define WALL "|"
@@ -14,15 +16,15 @@
 #define WIN 10
 
 // controls player 1
-#define UP1 'a'
-#define DOWN1 'z'
-#define UP1_H 'A'
-#define DOWN1_H 'Z'
+#define UP2 'a'
+#define DOWN2 'z'
+#define UP2_H 'A'
+#define DOWN2_H 'Z'
 // controls player 2
-#define UP2 'k'
-#define DOWN2 'm'
-#define UP2_H 'K'
-#define DOWN2_H 'M'
+#define UP1 'k'
+#define DOWN1 'm'
+#define UP1_H 'K'
+#define DOWN1_H 'M'
 
 
 // functions
@@ -32,6 +34,7 @@ void f_first_racket_physics(int* r1_y, char command);
 void f_second_racket_physics(int* r2_y, char command);
 int f_ball_physics(int* ball_x, int* ball_y, int* score1, int* score2, int* f_gr, int r1_x, int r1_y, int r2_x, int r2_y, int* impuls);
 void f_whoStart(int* score1, int* score2, int* impuls);
+int mygetch();
 
 int main(void) {
     // score
@@ -39,41 +42,40 @@ int main(void) {
     // x and y position
     int r1_y = 13, r2_y = 13, r1_x = 10, r2_x = WEIGHT - 10, ball_x = 41, ball_y = 13, f_gr = 0, impuls, f = 1;
 
-    initscr();
-    nodelay(stdscr, TRUE);
-    int speed = 30000;
-    while (score1 < WIN || score2 < WIN) {
+    int speed = 25000;
+
+    
+    while (score1 < WIN && score2 < WIN) {
+        char c1, c2;
+
         if (f == 1) {
             f_whoStart(&score1, &score2, &impuls);
             ball_x = 41;
             ball_y = 13;
         }
         usleep(speed * 4);
-        clear();
-
-        f_scorebar(score1, score2, WEIGHT);
-        f_render(HEIGHT, WEIGHT, r1_x, r1_y, r2_x, r2_y, ball_x, ball_y);
-
-        char c1 = getchar();
-        char c2 = getchar();
+        
+        c1 = mygetch();
 
         f_first_racket_physics(&r1_y, c1);
         f_second_racket_physics(&r2_y, c2);
 
+        f_scorebar(score1, score2, WEIGHT);
+        f_render(HEIGHT, WEIGHT, r1_x, r1_y, r2_x, r2_y, ball_x, ball_y);
+
         f = f_ball_physics(&ball_x, &ball_y, &score1, &score2, &f_gr, r1_x, r1_y, r2_x, r2_y, &impuls);
     }
     
-    if (score1 >= 21) {
+    if (score1 >= WIN) {
         f_scorebar(score1, score2, WEIGHT);
         printf("PLAYER 1 WON!\n");
+        return 0;
     }
-    if (score2 >= 21) {
+    if (score2 >= WIN) {
         f_scorebar(score1, score2, WEIGHT);
         printf("PLAYER 2 WON!\n");
+        return 0;
     }
-
-    nodelay(stdscr, FALSE);
-    endwin();
 
     return 0;
 }
@@ -128,19 +130,19 @@ void f_render(int h, int w, int r1_x, int r1_y, int r2_x, int r2_y, int b_x, int
 }
 
 void f_first_racket_physics(int* r1_y, char command) {
-    if ((command == UP1 || command == UP1_H) && (*r1_y < HEIGHT - 1)) {
+    if ((command == UP1 || command == UP1_H) && (*r1_y < HEIGHT - 2)) {
         *r1_y += 1;
     }
-    if ((command == DOWN1 || command == DOWN1_H) && (*r1_y > 2)) {
+    if ((command == DOWN1 || command == DOWN1_H) && (*r1_y > 3)) {
         *r1_y -= 1;
     }
 }
 
 void f_second_racket_physics(int* r2_y, char command) {
-    if ((command == UP2 || command == UP2_H) && (*r2_y < HEIGHT - 1)) {
+    if ((command == UP2 || command == UP2_H) && (*r2_y < HEIGHT - 2)) {
         *r2_y += 1;
     }
-    if ((command == DOWN2 || command == DOWN2_H) && (*r2_y > 2)) {
+    if ((command == DOWN2 || command == DOWN2_H) && (*r2_y > 3)) {
         *r2_y -= 1;
     }
 }
@@ -163,15 +165,17 @@ int f_ball_physics(int* ball_x, int* ball_y, int* score1, int* score2, int* f_gr
     }
     if (*ball_x == 2 || *ball_x == WEIGHT - 1) {
         if (*ball_x == 2) {
-            *score2 = *score1 + 1;
+            *score2 = *score2 + 1;
+            *f_gr = 0;
         } else {
-            *score1 = *score2 + 1;
+            *score1 = *score1 + 1;
+            *f_gr = 0;
         }
         return 1;
     }
 
     if (*f_gr <= 1) {
-        if (f_gr != 0) {
+        if ((*ball_x + *impuls == r1_x && *ball_y == r1_y) || (*ball_x + *impuls == r2_x && *ball_y == r2_y)) {
             *impuls = -(*impuls);
         }
         *ball_x += *impuls;
@@ -219,6 +223,19 @@ void f_whoStart(int* score1, int* score2, int* impuls) {
     if (*score1 == *score2) {
         *impuls = 1;
     }
+}
+
+int mygetch() {
+    struct termios oldt,
+    newt;
+    int ch;
+    tcgetattr( STDIN_FILENO, &oldt );
+    newt = oldt;
+    newt.c_lflag &= ~( ICANON | ECHO );
+    tcsetattr( STDIN_FILENO, TCSANOW, &newt );
+    ch = getchar();
+    tcsetattr( STDIN_FILENO, TCSANOW, &oldt );
+    return ch;
 }
 
 //bySAO
